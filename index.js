3317d3cbd5fd68269111a070d69bc6f264fbcc66
Sergio <sergiocmps7@gmail.com>
2021-09-11 10:46:25 -0600

    build

--- index.js
+++ index.js
@@ -32,9 +32,10 @@ app.get('/users', (req, res)=>{
     res.send('welcome to user page')
 })
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
